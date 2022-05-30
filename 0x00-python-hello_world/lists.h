1631b5fd7edd1b3cc4ef4ce06893592fb6bfd913
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-30 16:28:47 +0100

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,1 +1,18 @@
-hello
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
