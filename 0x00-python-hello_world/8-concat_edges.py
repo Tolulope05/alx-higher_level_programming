e06957840325075afc59c26e31b7b062ea66fbee
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-30 16:54:58 +0100

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,1 +1,5 @@
-hello
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
