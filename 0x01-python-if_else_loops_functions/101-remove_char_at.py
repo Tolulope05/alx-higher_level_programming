18faa75db4589eae103e40b451b3d4098a907475
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 16:09:12 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
+#!/usr/bin/python3
+# Author - bamidele Adefolaju
 
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
