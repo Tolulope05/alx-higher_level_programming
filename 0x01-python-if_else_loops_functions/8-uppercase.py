aff1ec6bb3fd4a07f19acdaff049e17f710eef91
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 16:20:18 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
-hello
+#!/usr/bin/python3
+# Author - Tolulope Fakunle
+
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
