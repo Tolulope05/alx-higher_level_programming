eaaeefdd3c82385a79548fee780b51cc6fb25c60
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 16:18:13 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,9 @@
-hello
+#!/usr/bin/python3
+# Author - Tolulope Fakunle
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
