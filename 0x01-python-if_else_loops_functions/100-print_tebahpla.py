61966f70cb7f03fb89a4d257e841ba01fc93a0dc
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 15:33:12 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,7 @@
-hello
+#!/usr/bin/python3
+# Author -Bamidele Adefolaju
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
