cd36a83bb1b5eba42065d705f947ee949d7723c4
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 16:08:28 +0100

    Update 101-main.py

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -1,1 +1,8 @@
-hello
+#!/usr/bin/env python3
+remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
