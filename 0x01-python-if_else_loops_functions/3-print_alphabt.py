14a2b599139fa3ba7f69a248f0e443097433e55a
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 16:14:26 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
-hello
+#!/usr/bin/python3
+# Author - Tolulope Fakunle
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
