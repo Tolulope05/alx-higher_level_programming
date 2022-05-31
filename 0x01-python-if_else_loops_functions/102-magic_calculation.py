a67bcb634cc195e3bc3065e5738e1ed114a6b3b8
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-31 15:35:25 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,1 +1,10 @@
-hello
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
