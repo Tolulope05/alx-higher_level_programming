190382b0be70fb8dc204b9fefb79dac4f3a59fcb
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-02 16:31:18 +0100

    Test file for addition function

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,1 +1,12 @@
-hi
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
