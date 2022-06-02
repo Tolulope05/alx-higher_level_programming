e42743f60d661ef919ed1d5cadc628b65b2b2e13
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-02 16:19:14 +0100

    Match bytecode provided by Holberton School

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,14 @@
-hi
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
