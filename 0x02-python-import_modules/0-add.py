2b562624d0c88e56d0ba0738946b4a9905b72d2e
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-02 16:14:51 +0100

    Print the sum of one and two

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,1 +1,9 @@
-hi
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
