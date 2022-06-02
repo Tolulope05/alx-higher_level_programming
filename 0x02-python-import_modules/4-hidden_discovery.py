3fe669b8434d6d88c6a2ec7689b3bd8bcf9b3fb7
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-02 16:23:06 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,10 @@
-hi
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
