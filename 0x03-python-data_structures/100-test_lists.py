4593c50989ce0d74e9df45d4300f1aa6604bf6e9
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 00:01:30 +0100

    Update 100-test_lists.py

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -1,1 +1,21 @@
-hi
+import ctypes
+
+lib = ctypes.CDLL('./libPyList.so')
+lib.print_python_list_info.argtypes = [ctypes.py_object]
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
