e4168a3f20249a05e0d4501acafc6a152b585ad2
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 00:05:41 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,7 @@
-hi
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
