c1bde738abbfc8ce8dfd7bb7b01f2a45281ed18f
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 00:06:08 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
-hi
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
