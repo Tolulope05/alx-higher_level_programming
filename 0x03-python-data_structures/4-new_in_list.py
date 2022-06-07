ea3c4ca03fa5fb6ee0e146d01042d5523489ced7
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 00:04:16 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
-hi
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
