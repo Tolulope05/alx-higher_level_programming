84c385391404e169c3d7bcaae94ddb707e61840c
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 18:13:33 +0100

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
-hi
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
