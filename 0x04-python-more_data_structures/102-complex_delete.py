c031ad28560f33950fcf05261dd4198d6a6264fa
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-08 18:07:54 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
-hi
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
