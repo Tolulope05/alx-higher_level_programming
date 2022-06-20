3d59117f4dd14430bf8e4d78b16b8fad1af4452f
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-20 16:04:14 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,13 @@
-Hi
+#!/usr/bin/python3
+from __future__ import print_function
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    else:
+        return res
