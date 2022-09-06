3c0bee7f649436084f8b635453ffaaf9767a7dba
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-09-06 23:50:42 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
