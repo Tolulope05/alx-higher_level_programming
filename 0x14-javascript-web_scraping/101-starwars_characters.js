c713110eb52900fee01dd29d48fbc9bc35be70a0
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-10-18 21:45:51 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    let characters = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
